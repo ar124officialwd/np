170673917d7d585f0e07da05a665541531553813
Ahmad Raza <pro.se.ahmad.raza.1@gmail.com>
2025-12-27 09:34:06 +0500

    chore: setup flake quality

--- flake.nix
+++ flake.nix
@@ -4,11 +4,28 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
+    alejandra.url = "github:kamadorueda/alejandra";
   };
 
-  outputs = { ... }: {
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
     nixvimModules = {
       base = import ./modules/neovim;
+    };
+
+    formatter.${system} = pkgs.alejandra;
+
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [pkgs.lefthook];
+
+      shellHook = ''
+        if [ ! -f .git/hooks/pre-commit ]; then
+          echo "Installing lefthook hooks..."
+          lefthook install
+        fi
+      '';
     };
   };
 }
